10dbc4e3c09cc0ec6d986ea8d4f95afb48ea19d0
pacmanface <pacmanface@yandex.ru>
2022-01-07 21:32:49 +0200

    myRMI 2

--- staticFoo/src/com/headfirst/pacmanface/Foo5.java
+++
@@ -1,9 +0,0 @@
-public class Foo5 {
-    
-    static final int x = 5;
-    
-    void go(final int x){
-         System.out.println(x);   
-    }
-    
-}
--- staticFoo/src/com/headfirst/pacmanface/Foo6.java
+++
@@ -1,9 +0,0 @@
-public class Foo6 {
-
-    int x = 6;
-
-    static void go(final int x){
-        System.out.println(x);
-    }
-    
-}
