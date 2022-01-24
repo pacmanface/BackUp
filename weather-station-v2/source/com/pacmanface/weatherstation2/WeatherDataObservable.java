19cfe23354391c8dcd945c29d31bca821fc48153
pacmanface <pacmanface@yandex.ru>
2022-01-24 17:35:29 +0200

    try weather-station 2. not working. good

---
+++ weather-station-v2/source/com/pacmanface/weatherstation2/WeatherDataObservable.java
@@ -0,0 +1,50 @@
+package com.pacmanface.weatherstation2;
+
+import java.util.*;
+
+public class WeatherDataObservable implements Observable,Runnable{
+
+	private ArrayList<Observer> observers = new ArrayList<>();
+	public static volatile boolean isActual;
+	
+	public WeatherDataObservable(){
+	}
+	
+	@Override
+	public void run() {
+		while(true) checkData();
+	}
+
+	public void checkData(){
+		if(!isActual) notifyObservers();
+	}
+
+	public void addObserver(Observer o){
+		observers.add(o);
+		notifyObserver(o);
+	}
+	
+	public void deleteObserver(Observer o){
+		if(!observers.isEmpty()){		
+			try{
+				observers.remove(o);
+			}catch(Exception ex){
+				System.out.println("can't find the observer!");
+				ex.printStackTrace();
+			}
+		}else System.out.println("no observers");	
+	}
+	
+	public void notifyObservers(){
+		if(!observers.isEmpty()){
+			for(Observer o:observers){
+				o.update();
+			}
+			isActual=true;		
+		}else System.out.println("Noone to notify");
+	}
+	
+	public void notifyObserver(Observer o){
+		o.update();
+	}
+}
