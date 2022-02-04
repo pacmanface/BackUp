8f67dfa1d75e1272b93e7c97d422f346d4361715
pacmanface <pacmanface@yandex.ru>
2022-02-04 13:07:17 +0200

    uncompiled Beverage

---
+++ template-beverage/source/com/pacamaface/templatebeverage/GreenTea.java
@@ -0,0 +1,12 @@
+package com.pacmanface.templatebeverage;
+
+class GreenTea extends HotBeverage{
+	public void brew(){
+		System.out.println("brew the perfect green tea");
+	}
+	public void addCondiment(){
+		System.out.println("add the bitter lemon slice to the tea.");
+	}
+	
+	
+}
---
+++ template-beverage/source/com/pacamaface/templatebeverage/HotBeverage.java
@@ -0,0 +1,30 @@
+package com.pacmanface.templatebeverage;
+
+abstract class HotBeverage{
+
+	//protected Feedback f;
+
+	public final void prepareReceipt(){
+		boilWater();
+		brewBeverage();
+		pourInCup();
+		if(isAddCondiment) addCondiment();
+	}
+	
+	public void boilWater(){
+		System.out.println("boil the water");
+	}
+	
+	abstract void brewBeverage();
+	
+	public void pourInCup(){
+		System.out.println("pour the beverage in the cup");
+	}
+	
+	abstract void addCondiment();
+	
+	public boolean isAddCondiment(){
+		//return f.askUser();
+		return true;
+	}
+}
