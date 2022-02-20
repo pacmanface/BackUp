18326e8a4710aeab8a67166c26a45fed850cc879
pacmanface <pacmanface@yandex.ru>
2022-02-20 11:43:18 +0200

    huinder task

---
+++ personbean-proxy-filter/source/com/pacmanface/huinder/OwnerInvocationHandler.java
@@ -0,0 +1,1 @@
+package com.pacmanface.huinder;
---
+++ personbean-proxy-filter/source/com/pacmanface/huinder/PersonBeanImpl.java
@@ -0,0 +1,52 @@
+package com.pacmanface.huinder;
+
+public interface PersonBeanImpl{
+	private String name;
+	private String gender;
+	private String interests;
+	private int rating = 0;
+	private int ratingCount = 0;
+	
+	@Override
+	public String getName(){
+		return name;
+	}
+	
+	@Override
+	public String getGender(){
+		return gender;
+	}
+	
+	@Override
+	public String getInterests(){
+		return interests;
+	}
+	
+	@Override
+	public int getHotOrNotRating(){
+		return rating == 0 ? 0 : (int)rating/ratingCount;
+	}
+	
+	@Override
+	public void setName(String n){
+		name = n;
+	}
+	
+	@Override
+	public void setGender(String g){
+		gender = g;
+	}
+	
+	@Override
+	public void setInterests(String i){
+		interests = i;
+	}
+	
+	@Override
+	public void setHotOrNotRating(int r){
+		if(r>0){ 
+			rating+=r;
+			ratingCount++;
+		}
+	}
+}
